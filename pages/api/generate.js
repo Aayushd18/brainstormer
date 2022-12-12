07bfed5c64d9b1e895e9a057f995ad8d17e9d085
Aayushd18 <deshmukhaayush81@gmail.com>
2022-12-12 22:33:34 +0530

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,121 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `Idea:`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${req.body.userInput}
+
+  ${basePromptPrefix}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${req.body.userInput}
+
+    ${basePromptPrefix} `,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  console.log(basePromptOutput)
+
+  const marketInfoPrompt = `${req.body.userInput}
+
+  ${basePromptPrefix} 
+  
+  ${basePromptOutput.text}
+  
+  Based on above startup idea How will it impact the healthcare market: `;
+  console.log(marketInfoPrompt)
+
+  const marketInfoCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: marketInfoPrompt,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+
+  const marketInfoOutput = marketInfoCompletion.data.choices.pop();
+  console.log(marketInfoOutput)
+
+  const problemSolvePrompt = `${marketInfoPrompt}
+  
+  ${marketInfoOutput.text}
+  
+  Based on above info What problem does this idea exactly solves: `
+
+  const problemSolveCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: problemSolvePrompt,
+    temperature: 0.7,
+    max_tokens: 250,
+  })
+
+  const problemSolveOutput = problemSolveCompletion.data.choices.pop();
+  console.log(problemSolveOutput)
+
+  const impactPrompt = `${problemSolvePrompt}
+
+  ${problemSolveOutput.text}
+  
+  Based on the above data How this impacts common people: `;
+
+  const impactCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: impactPrompt,
+    temperature: 0.7,
+    max_tokens: 250
+  });
+
+  const impactOutput = impactCompletion.data.choices.pop()
+  console.log("Impact............", impactOutput);
+
+  const existingCompanyPrompt = `${impactPrompt}
+  
+  ${impactOutput.text}
+  
+  Based on the above data Is there any companies working on this idea: `;
+
+  const existingCompanyCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: existingCompanyPrompt,
+    temperature: 0.7,
+    max_tokens: 250
+  });
+
+  const existingCompanyOutput = existingCompanyCompletion.data.choices.pop();
+  console.log(existingCompanyOutput);
+
+  const namePrompt = `${existingCompanyPrompt}
+  
+  ${existingCompanyOutput.text}
+  
+  Based on the above data suggest a unique cool name for the startup: `;
+
+  const namePromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: namePrompt,
+    temperature: 0.7,
+    max_tokens: 250
+  });
+
+  const nameOutput = namePromptCompletion.data.choices.pop();
+  console.log("Name........", nameOutput)
+
+  res.status(200).json({ 
+    name: nameOutput.text,
+    idea: basePromptOutput.text,
+    marketInfo: marketInfoOutput.text,
+    problemSolve: problemSolveOutput.text,
+    impact: impactOutput.text,
+    existingCompany: existingCompanyOutput.text
+  });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,93 @@
+import  { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [ apiOutput, setApiOutput] = useState();
+  const [isGenerating, setIsGenerating] = useState(false);
+
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true)
+    const response = await fetch('api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput })
+    });
+
+    const data = await response.json();
+    console.log(data)
+
+    setApiOutput(data);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Brain Stormer</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Brainstormer</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Get your next startup idea</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="Give me one startup idea based on X in the Y sector." className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <div>
+                  <h1>Your Startup Name: <span className='startup-name'>{apiOutput.name}</span></h1>
+                  <div className='data-section'>
+                    <p className='question-title'>Idea:</p>
+                    <p>{apiOutput.idea}</p>
+                  </div>
+                  <div className='data-section'>
+                    <p className='question-title'>How will the above idea impact the healthcare market:</p>
+                    <p>{apiOutput.marketInfo}</p>
+                  </div>
+                  <div className='data-section'>
+                    <p className='question-title'>What problems does this idea really solves:</p>
+                    <p>{apiOutput.problemSolve}</p>
+                  </div>
+                  <div className='data-section'>
+                    <p className='question-title'>How this impacts common people:</p>
+                    <p>{apiOutput.impact}</p>
+                  </div>
+                  <div className='data-section'>
+                    <p className='question-title'>Any companies working on this idea:</p>
+                    <p>{apiOutput.existingCompany}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
